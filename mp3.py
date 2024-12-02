72c5d8aeeaa40c28bb9b29e65888581502a9bd12
Jojo aka Lil Broomstikk <90873840+zoey9015@users.noreply.github.com>
2024-12-02 12:48:30 -0500

    Update and rename itzme.md to mp3.py

--- mp3.py
+++ mp3.py
@@ -1,7 +1,4 @@
-- 👋 Hi, I’m @zoey9015
-- 👀 I’m interested in ...coding and graphic design
-- 🌱 I’m currently learning ...php & python
-- 📫 How to reach me ...zoey9015@gmail.com
+mp3 WIP
 
 <!---
 zoey9015/zoey9015 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
